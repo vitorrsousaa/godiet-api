3db320b21a0eda1bcaaff6e2e561715b1774441b
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-27 11:46:16 -0300

    started project

---
+++ src/server/functions/getUserById.ts
@@ -0,0 +1,11 @@
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "User by ID",
+      user: event.pathParameters?.userId,
+    }),
+  };
+}
