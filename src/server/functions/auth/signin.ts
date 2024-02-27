6680a47fc0ac4330bd32d719adf20a9a8db2270f
Vitor Sousa <v.sousa.cf@gmail.com>
2024-02-27 17:02:56 -0300

    fix: fix cors and remove package

--- src/server/functions/auth/signin.ts
+++ src/server/functions/auth/signin.ts
@@ -1,17 +1,12 @@
 import { requestAdapter, responseAdapter } from '@/adapters';
 import { makeControllerSignin } from '@/factories/controllers/makeControllerSignin';
 
-import middy from '@middy/core';
-import cors from '@middy/http-cors';
-
 import type { APIGatewayProxyEventV2 } from 'aws-lambda';
 
-async function lambdaHandler(event: APIGatewayProxyEventV2) {
+export async function handler(event: APIGatewayProxyEventV2) {
   const controller = makeControllerSignin();
 
   const response = await controller.handle(requestAdapter(event));
 
   return responseAdapter(response);
 }
-
-export const handler = middy().use(cors()).handler(lambdaHandler);
