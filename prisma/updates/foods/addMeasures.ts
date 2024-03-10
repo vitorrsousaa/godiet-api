ba6a0240f72c4bc9ab32e088cf7b31a9d13abfdf
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-10 19:37:16 -0300

    chore: create scripts to update database

---
+++ prisma/updates/foods/addMeasures.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client';
+
+import foodList from '../../data/foodList.json';
+
+const prisma = new PrismaClient();
+
+export async function addMeasures() {
+  const foods = await prisma.food.findMany();
+
+  let countUpdated = 0;
+  const foodsWithoutMeasures: unknown[] = [];
+
+  foods.forEach(async (food) => {
+    const originalFood = foodList.find((list) => list.name === food.name);
+    let measures = [];
+
+    if (!originalFood) {
+      foodsWithoutMeasures.push(food);
+      measures = [{ name: 'gramas', qty: 1 }];
+    } else {
+      measures = originalFood.measures;
+    }
+
+    await prisma.food
+      .update({
+        where: {
+          id: food.id,
+        },
+        data: {
+          measures,
+        },
+      })
+      .catch((error) => {
+        console.log('Error', food.name);
+        console.log(error);
+      })
+      .then(() => {
+        countUpdated = countUpdated + 1;
+      });
+  });
+
+  console.log('Updated:', countUpdated);
+  console.log('With error:', foodsWithoutMeasures);
+}
+
+addMeasures();
