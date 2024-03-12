c25357165f02914fa0f8cab3e71b1d36b104e77c
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-12 18:05:58 -0300

    fix: update database

---
+++ prisma/migrations/20240312210427_delete_meal_food/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "meal_foods" DROP CONSTRAINT "meal_foods_meal_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "meal_foods" ADD CONSTRAINT "meal_foods_meal_id_fkey" FOREIGN KEY ("meal_id") REFERENCES "meals"("id") ON DELETE CASCADE ON UPDATE CASCADE;
