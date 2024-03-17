58fa03507730af115af555b4734e8c0b7867ed38
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-17 16:44:58 -0300

    feat: include new methods on favorite meal repository

--- src/app/database/repositories/favoritesMeal/repository.ts
+++ src/app/database/repositories/favoritesMeal/repository.ts
@@ -6,6 +6,8 @@ export interface IFavoriteMealRepositories {
   findAll(
     findAllArgs: Prisma.FavoriteMealFindManyArgs
   ): Promise<TFavoriteMeal[]>;
+  delete(deleteArgs: Prisma.FavoriteMealDeleteArgs): Promise<TFavoriteMeal>;
+  create(createArgs: Prisma.FavoriteMealCreateArgs): Promise<TFavoriteMeal>;
 }
 
 class FavoriteMealRepositories implements IFavoriteMealRepositories {
@@ -13,6 +15,14 @@ class FavoriteMealRepositories implements IFavoriteMealRepositories {
 
   async findAll(findAllArgs: Prisma.FavoriteMealFindManyArgs) {
     return this.prismaService.favoriteMeal.findMany(findAllArgs);
+  }
+
+  async delete(deleteArgs: Prisma.FavoriteMealDeleteArgs) {
+    return this.prismaService.favoriteMeal.delete(deleteArgs);
+  }
+
+  async create(createArgs: Prisma.FavoriteMealCreateArgs) {
+    return this.prismaService.favoriteMeal.create(createArgs);
   }
 }
 
