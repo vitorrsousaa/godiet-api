58049015ac02aaf0c015a0f42c1013c71bd1e8ee
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-18 07:08:47 -0300

    feat: include function that calculate each attribute

--- src/app/modules/favoriteMeal/services/FindAll/service.ts
+++ src/app/modules/favoriteMeal/services/FindAll/service.ts
@@ -1,4 +1,5 @@
 import { TFavoriteMeal } from '@/entities/favoriteMeal';
+import { TAttribute, TMeasure } from '@/entities/food';
 import { IFavoriteMealRepositories } from '@/repositories/favoritesMeal';
 
 import * as z from 'zod';
@@ -17,6 +18,27 @@ export interface IFindAllService {
   execute(findAllInput: IFindAllInput): Promise<IFindAllOutput>;
 }
 
+interface CalculateAttributesInput {
+  baseQty: number;
+  attribute: TAttribute;
+  qty: number;
+  measure: TMeasure;
+}
+
+function calculateAttributes(
+  calculateAttributesInput: CalculateAttributesInput
+): TAttribute {
+  const { attribute, baseQty, qty, measure } = calculateAttributesInput;
+
+  const totalQty = qty * measure.qty;
+
+  return {
+    name: attribute.name,
+    unit: attribute.unit,
+    qty: (attribute.qty * totalQty) / baseQty,
+  };
+}
+
 export class FindAllService implements IFindAllService {
   constructor(
     private readonly favoriteMealRepositories: IFavoriteMealRepositories
@@ -25,10 +47,48 @@ export class FindAllService implements IFindAllService {
   async execute(findAllInput: IFindAllInput): Promise<IFindAllOutput> {
     const { userId } = findAllInput;
 
-    return this.favoriteMealRepositories.findAll({
+    const allFavorites = await this.favoriteMealRepositories.findAll({
       where: {
         userId,
       },
+      include: {
+        mealFoods: {
+          include: {
+            food: true,
+          },
+        },
+      },
     });
+
+    return allFavorites.map(this.mapperMealFoods);
+  }
+
+  private mapperMealFoods(favoriteMeal: TFavoriteMeal): TFavoriteMeal {
+    const { mealFoods } = favoriteMeal;
+
+    const newMealFoods = mealFoods.map((mealFood) => {
+      const { food, ...restOfMealFood } = mealFood;
+      const { attributes, ...restOfFoods } = food;
+
+      return {
+        ...restOfMealFood,
+        food: {
+          ...restOfFoods,
+          attributes: attributes.map((attribute) =>
+            calculateAttributes({
+              attribute,
+              baseQty: food.baseQty,
+              qty: mealFood.qty,
+              measure: mealFood.measure,
+            })
+          ),
+        },
+      };
+    });
+
+    return {
+      ...favoriteMeal,
+      mealFoods: newMealFoods,
+    };
   }
 }
