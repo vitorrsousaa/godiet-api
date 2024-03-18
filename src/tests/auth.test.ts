6c0dc871acb6186141b81b1c0535dc3a8296e736
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-18 08:54:32 -0300

    feat: initialize model to create test of integration

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,6 +1,8 @@
 // src/tests/auth.test.ts
 import { describe, expect, it } from 'vitest';
 
+import { handler } from '../server/functions/auth/signup';
+
 import prisma from './helpers/prisma';
 
 describe('/auth', async () => {
@@ -17,6 +19,39 @@ describe('/auth', async () => {
       console.log(user);
 
       expect(user.name).toBe('Joãozinho');
+    });
+
+    it('Should signup', async () => {
+      const response = await handler({
+        headers: {},
+        isBase64Encoded: false,
+        rawPath: '/auth/signup',
+        rawQueryString: '',
+        routeKey: 'POST /auth/signup',
+        version: '2.0',
+        requestContext: {
+          accountId: 'offlineContext_accountId',
+          apiId: 'offlineContext_apiId',
+          domainName: 'offlineContext_domainName',
+          domainPrefix: 'offlineContext_domainPrefix',
+          http: {
+            method: 'POST',
+            path: '/auth/signup',
+            protocol: 'HTTP/1.1',
+            sourceIp: '::1',
+            userAgent: 'Insomnia/2023.5.6',
+          },
+          requestId: 'offlineContext_resourceId',
+          routeKey: 'POST /auth/signup',
+          stage: '$default',
+          time: '18/Mar/2024:08:31:44 -0300',
+          timeEpoch: 1710761504185,
+        },
+      });
+
+      console.log(response);
+
+      expect(Boolean(response)).toBeTruthy();
     });
   });
 });
