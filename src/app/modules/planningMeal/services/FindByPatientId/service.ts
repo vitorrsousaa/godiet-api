de6908e2f76b1341ee66fce1a819b65d89431503
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-24 16:03:55 -0300

    feat(GODIET-82): :sparkles: remove food utils and increase planning summary

--- src/app/modules/planningMeal/services/FindByPatientId/service.ts
+++ src/app/modules/planningMeal/services/FindByPatientId/service.ts
@@ -1,5 +1,5 @@
-import { TPlanningMeal } from '@/entities/planningMeal';
-import { IFoodUtils } from '@/modules/food/utils/food';
+import { TPlanningMealWithMeals } from '@/entities/planningMeal';
+import { calculateAttributes } from '@/modules/food/utils/food';
 import { IPlanningMealRepositories } from '@/repositories/planningMeal';
 
 import * as z from 'zod';
@@ -15,9 +15,20 @@ export type TFindByPatientId = z.infer<typeof FindByPatientIdServiceSchema>;
 
 export type IFindByPatientIdInput = TFindByPatientId;
 
-export type TPlanningOutput = TPlanningMeal;
+export type TPlanningOutput = TPlanningMealWithMeals;
 
-export type IFindByPatientIdOutput = TPlanningOutput[];
+export type TSummary = {
+  prot: number;
+  fat: number;
+  carb: number;
+  energy: number;
+};
+
+export type TPlanningMealWithSummary = TPlanningOutput & {
+  summary: TSummary;
+};
+
+export type IFindByPatientIdOutput = TPlanningMealWithSummary[];
 
 export interface IFindByPatientIdService {
   execute(
@@ -27,8 +38,7 @@ export interface IFindByPatientIdService {
 
 export class FindByPatientIdService implements IFindByPatientIdService {
   constructor(
-    private readonly planningMealRepositories: IPlanningMealRepositories,
-    private readonly foodUtils: IFoodUtils
+    private readonly planningMealRepositories: IPlanningMealRepositories
   ) {}
 
   async execute(
@@ -37,7 +47,7 @@ export class FindByPatientIdService implements IFindByPatientIdService {
     const { userId, patientId } = findByPatientIdInput;
 
     try {
-      const planning = (await this.planningMealRepositories.findAll({
+      const plannings = (await this.planningMealRepositories.findAll({
         where: {
           userId,
           patientId,
@@ -55,9 +65,105 @@ export class FindByPatientIdService implements IFindByPatientIdService {
         },
       })) as unknown as TPlanningOutput[];
 
-      return planning;
-    } catch {
+      const planningMealsWithCalculated = plannings.map(
+        this.mapperPlanningMeal
+      );
+
+      return planningMealsWithCalculated.map(this.getCalculateSummary);
+    } catch (error) {
       throw new InvalidPlanningId();
     }
+  }
+
+  private mapperPlanningMeal(
+    planningMeal: TPlanningMealWithMeals
+  ): TPlanningMealWithMeals {
+    const { createdAt, id, meals, name, patientId, updatedAt, userId } =
+      planningMeal;
+
+    return {
+      createdAt,
+      id,
+      name,
+      patientId,
+      updatedAt,
+      userId,
+      meals: meals.map((meal) => {
+        return {
+          ...meal,
+          mealFoods: meal.mealFoods.map((mealFood) => {
+            const { food, ...restOfMealFood } = mealFood;
+            const { attributes, ...restOfFoods } = food;
+
+            return {
+              ...restOfMealFood,
+              food: {
+                ...restOfFoods,
+                attributes: attributes.map((attribute) =>
+                  calculateAttributes({
+                    attribute,
+                    baseQty: food.baseQty,
+                    qty: mealFood.qty,
+                    measure: mealFood.measure,
+                  })
+                ),
+              },
+            };
+          }),
+        };
+      }),
+    };
+  }
+
+  private getCalculateSummary(
+    planningMeal: TPlanningMealWithMeals
+  ): TPlanningMealWithSummary {
+    const { meals } = planningMeal;
+
+    const summary = meals.reduce(
+      (acc, meal) => {
+        const mealSummary = meal.mealFoods.reduce(
+          (acc, mealFood) => {
+            const { food } = mealFood;
+            const proteinAttribute = food.attributes.find(
+              (attribute) => attribute.name === 'protein'
+            );
+
+            const fatAttribute = food.attributes.find(
+              (attribute) => attribute.name === 'lipid'
+            );
+
+            const carbAttribute = food.attributes.find(
+              (attribute) => attribute.name === 'carbohydrate'
+            );
+
+            const energyAttribute = food.attributes.find(
+              (attribute) => attribute.name === 'energy'
+            );
+
+            return {
+              prot: acc.prot + (proteinAttribute?.qty || 0),
+              fat: acc.fat + (fatAttribute?.qty || 0),
+              carb: acc.carb + (carbAttribute?.qty || 0),
+              energy: acc.energy + (energyAttribute?.qty || 0),
+            };
+          },
+          { prot: 0, fat: 0, carb: 0, energy: 0 }
+        );
+
+        return {
+          prot: acc.prot + mealSummary.prot,
+          fat: acc.fat + mealSummary.fat,
+          carb: acc.carb + mealSummary.carb,
+          energy: acc.energy + mealSummary.energy,
+        };
+      },
+      { prot: 0, fat: 0, carb: 0, energy: 0 }
+    );
+
+    return {
+      ...planningMeal,
+      summary,
+    };
   }
 }
