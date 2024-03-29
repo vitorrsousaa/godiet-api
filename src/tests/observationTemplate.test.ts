33df0a13eb6bba40fc05ba89e4e38677e9b8bb5c
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-29 11:24:09 -0300

    test(GODIET-97): :white_check_mark: fixes observation template test of integration with delete

--- src/tests/observationTemplate.test.ts
+++ src/tests/observationTemplate.test.ts
@@ -2,31 +2,33 @@
 import { describe, expect, it } from 'vitest';
 
 import { handler as handlerSignUp } from '../server/functions/auth/signup';
-import { handler } from '../server/functions/observationTemplate/create';
+import { handler as handlerCreate } from '../server/functions/observationTemplate/create';
+import { handler as handlerDelete } from '../server/functions/observationTemplate/delete';
 
 import { IInvoke, Invoke } from './helpers/invokeFunction';
 
 describe('/observation-template', () => {
-  describe('[POST] /observation-template/create', async () => {
-    let invoke: IInvoke;
-    let accessToken: string;
+  let accessToken: string;
 
-    beforeAll(async () => {
-      invoke = new Invoke(handler, 'POST /observation-template/create');
+  beforeEach(async () => {
+    const invokeSignUp = new Invoke(handlerSignUp, 'POST /auth/signup');
+    const response = await invokeSignUp.execute({
+      body: {
+        email: 'user@email.com.br',
+        password: '123456789',
+        name: 'User Name',
+        phone: '21998217563',
+      },
     });
 
-    beforeEach(async () => {
-      const invokeSignUp = new Invoke(handlerSignUp, 'POST /auth/signup');
-      const response = await invokeSignUp.execute({
-        body: {
-          email: 'user@email.com.br',
-          password: '123456789',
-          name: 'User Name',
-          phone: '21998217563',
-        },
-      });
+    accessToken = response.body.accessToken as string;
+  });
 
-      accessToken = response.body.accessToken as string;
+  describe('[POST] /observation-template', async () => {
+    let invoke: IInvoke;
+
+    beforeAll(async () => {
+      invoke = new Invoke(handlerCreate, 'POST /observation-template');
     });
 
     it('Should create an observation template', async () => {
@@ -39,14 +41,54 @@ describe('/observation-template', () => {
         headers: { authorization: `Bearer ${accessToken}` },
       });
 
-      console.log(response.body);
-
       expect(response.statusCode).toBe(200);
       expect(response.body).toMatchObject({
         id: expect.any(String),
         title: 'Title',
         text: 'Text',
       });
+    });
+  });
+
+  describe('[DELETE] /observation-template/{id}', async () => {
+    let invoke: IInvoke;
+    let observationTemplateId: string;
+
+    beforeAll(async () => {
+      invoke = new Invoke(handlerDelete, 'DELETE /observation-template/{id}');
+    });
+
+    beforeEach(async () => {
+      const invokeCreate = new Invoke(
+        handlerCreate,
+        'POST /observation-template'
+      );
+      const body = {
+        title: 'Title',
+        text: 'Text',
+      };
+      const created = await invokeCreate.execute({
+        body,
+        headers: { authorization: `Bearer ${accessToken}` },
+      });
+
+      console.log({ created });
+
+      observationTemplateId = created.body.id as string;
+    });
+
+    it('Should delete observation template', async () => {
+      console.log({ observationTemplateId });
+      const response = await invoke.execute({
+        body: {},
+        headers: { authorization: `Bearer ${accessToken}` },
+        params: { id: observationTemplateId },
+      });
+
+      console.log({ response: response.body });
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toBeNull();
     });
   });
 });
