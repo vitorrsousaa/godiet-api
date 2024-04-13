b84b2c6f965f725172a8c14f468f5140150b92ac
Vitor Sousa <v.sousa.cf@gmail.com>
2024-04-12 21:14:14 -0300

    feat(GODIET-111): :clown_face: update observation template with new texts

--- src/app/constants/observationTemplate.ts
+++ src/app/constants/observationTemplate.ts
@@ -6,68 +6,34 @@ export type ITemplates = {
 export const templates: ITemplates[] = [
   {
     title: 'Atenção plena durante às refeições (goDiet)',
-    text: 'Comer devagar e mastigar com calma são passos importantes para boa digestão e para observar seus sinais de saciedade, diminuindo a chance de comer em excesso. Coma com atenção e desfrute o que está comendo, sem se envolver em outra atividade. Procure comer em locais limpos, confortáveis e tranquilos e onde não haja estímulos para o consumo excessivo de alimentos. Evite comer assistindo TV, mexendo no celular ou lendo, pois isso pode levar a uma distração e a uma alimentação desatenta.',
+    text: '<p>Comer devagar e mastigar com calma são passos importantes para boa digestão e para observar seus sinais de saciedade, diminuindo a chance de comer em excesso. Coma com atenção e desfrute o que está comendo, sem se envolver em outra atividade. Procure comer em locais limpos, confortáveis e tranquilos e onde não haja estímulos para o consumo excessivo de alimentos. Evite comer assistindo TV, mexendo no celular ou lendo, pois isso pode levar a uma distração e a uma alimentação desatenta.</p>',
   },
   {
     title: 'Consumo de café após as refeições (goDiet)',
-    text: 'ATENÇÃO: A ingestão do café deverá ser feita no mínimo 45 minutos após a refeição, pois o café pode diminuir a absorção de alguns nutrientes importantes como o ferro.',
+    text: '<p><strong>ATENÇÃO</strong>: A ingestão do café deverá ser feita no mínimo 45 minutos <strong>após</strong> a refeição, pois o café pode diminuir a absorção de alguns nutrientes importantes como o ferro.</p>',
   },
   {
     title: 'Remolho das leguminosas (goDiet)',
-    text: `
-    Realizar o remolho das leguminosas traz alguns benefícios nos quais incluem:
-
-- Melhora da digestibilidade dos grãos, e assim, diminuição dos desconfortos gastrointestinais.
-- Diminuição no tempo de cozimento, ou seja, os grãos ficam cozidos de forma mais rápida quando comparados àqueles que não ficaram no remolho.
-- Diminuição de fatores anti-nutricionais que prejudicam a absorção de alguns nutrientes, como: ferro e zinco.
-
-Como realizar o remolho?
-
-1) Colocar a leguminosa (feijão, grão de bico, lentilha, por exemplo) em uma peneira e lavar com água corrente;
-
-2) Feito isso, coloque os grãos em um recipiente e cubra com água até ficarem completamente submersos;
-
-3) Deixe descansar por pelo menos 8 a 12 horas, e caso seja possível, troque a água durante esse período;
-
-4) Após deixar descansar pelo tempo necessário, enxague os grãos e leve ao cozimento.
-    `,
+    text: '<p>Realizar o remolho das leguminosas traz alguns benefícios nos quais incluem:</p><ul><li><p>Melhora da digestibilidade dos grãos, e assim, diminuição dos desconfortos gastrointestinais.</p></li><li><p>Diminuição no tempo de cozimento, ou seja, os grãos ficam cozidos de forma mais rápida quando comparados àqueles que não ficaram no remolho.</p></li><li><p>Diminuição de fatores anti-nutricionais que prejudicam a absorção de alguns nutrientes, como: ferro e zinco.</p></li></ul><p><strong>Como realizar o remolho?</strong></p><p>1) Colocar a leguminosa (feijão, grão de bico, lentilha, por exemplo) em uma peneira e lavar com água corrente;</p><p>2) Feito isso, coloque os grãos em um recipiente e cubra com água até ficarem completamente submersos;</p><p>3) Deixe descansar por pelo menos 8 a 12 horas, e caso seja possível, troque a água durante esse período; </p><p>4) Após deixar descansar pelo tempo necessário, enxague os grãos e leve ao cozimento.</p>',
   },
   {
     title: 'Infusão e decocção de chás (goDiet)',
-    text: `
-    INDUSTRIALIZADOS:
-    Os chás de lata e de caixinha têm pequenas quantidades da erva e, muitas vezes, têm açúcar e outras substâncias que só realçam o sabor. Os princípios ativos com funções terapêuticas estão em menor quantidade, por isso é importante optar pela erva fresca ou seca.
-
-    PREPARO:
-    - O chá pode ser preparado de duas maneiras: infusão ou decocção.
-
-    Infusão:
-    Para a infusão coloque em média 1 colher de sopa cheia da erva para 1 litro de água, ou 1 colher de chá da erva para 1 xícara de água. Deixe a erva seca na água quente de 2 a 10 minutos. Logo após, coe e beba. Em geral as partes utilizadas para infusão são: folhas, flores e plantas aromáticas.
-
-    Decocção:
-    Para a decocção coloque as cascas, por exemplo, em um recipiente com água fria e leve ao fogo. Aqueça por aproximadamente 10 a 15 minutos até ferver. Logo após, coe e beba. Em geral as partes utilizadas para decocção são: cascas, raízes e sementes.
-
-    - Para melhorar a aceitação devido ao gosto amargo de algumas ervas, uma boa dica é adicionar uma rama de canela na preparação.
-    `,
+    text: '<p><strong>INDUSTRIALIZADOS</strong>:</p><p>Os chás de lata e de caixinha têm pequenas quantidades da erva e, muitas vezes, têm açúcar e outras substâncias que só realçam o sabor. Os princípios ativos com funções terapêuticas estão em menor quantidade, por isso é importante optar pela erva fresca ou seca.</p><p><strong>PREPARO:</strong></p><p>- O chá pode ser preparado de duas maneiras: infusão ou decocção.</p><p><em>Infusão:</em></p><p>Para a infusão coloque em média 1 colher de sopa cheia da erva para 1 litro de água, ou 1 colher de chá da erva para 1 xícara de água. Deixe a erva seca na água quente de 2 a 10 minutos. Logo após, coe e beba. Em geral as partes utilizadas para infusão são: folhas, flores e plantas aromáticas.</p><p><em>Decocção:</em></p><p>Para a decocção coloque as cascas, por exemplo, em um recipiente com água fria e leve ao fogo. Aqueça por aproximadamente 10 a 15 minutos até ferver. Logo após, coe e beba. Em geral as partes utilizadas para decocção são: cascas, raízes e sementes.</p><p>- Para melhorar a aceitação devido ao gosto amargo de algumas ervas, uma boa dica é adicionar uma rama de canela na preparação.</p><p>- Para que não haja competição de possíveis fatores anti-nutricionais com os nutrientes vindo dos alimentos, evite ingerir chás após as grandes refeições.</p>',
   },
   {
     title: 'Higiene dos alimentos (goDiet)',
-    text: 'Para higienizar seus legumes e verduras, primeiro lave um a um em água corrente com cuidado. Em seguida, prepare um solução clorada com 1 litro de água e 10 ml (1 colher de sopa rasa) de água sanitária a 2,5%. A água sanitária deve ser sem perfume e com recomendações para uso em alimentos. Coloque os vegetais já lavados de molho na solução de água clorada por 15 minutos, depois enxague-os para retirar o excesso de cloro.',
+    text: '<p>Para higienizar seus legumes e verduras, primeiro lave um a um em água corrente com cuidado. Em seguida, prepare um solução clorada com 1 litro de água e 10 ml (1 colher de sopa rasa) de água sanitária a 2,5%. A água sanitária deve ser sem perfume e com recomendações para uso em alimentos. Coloque os vegetais já lavados de molho na solução de água clorada por 15 minutos, depois enxague-os para retirar o excesso de cloro.</p>',
   },
   {
     title: 'Hidratação no treino (goDiet)',
-    text: 'Hidrate-se durante o treino. Beba no mínimo 400ml de água.',
+    text: '<p>Hidrate-se durante o treino. Beba no mínimo 400ml de água.</p>',
   },
   {
     title: 'Evite a monotonia alimentar (goDiet)',
-    text: 'Uma alimentação variada, diversificada, equilibrada e individualizada contribui para prevenção de deficiências nutricionais, auxilia na manutenção da saúde e protege contra doenças crônicas não transmissíveis, como: obesidade, diabetes, hipertensão, doenças cardiovasculares e respiratórias, dentre outras. Por isso, é importante evitar a monotonia alimentar e variar os alimentos conforme orientado no seu planejamento alimentar.',
+    text: '<p>Uma alimentação variada, diversificada, equilibrada e individualizada contribui para prevenção de deficiências nutricionais, auxilia na manutenção da saúde e protege contra doenças crônicas não transmissíveis, como: obesidade, diabetes, hipertensão, doenças cardiovasculares e respiratórias, dentre outras. Por isso, é importante evitar a monotonia alimentar e variar os alimentos conforme orientado no seu planejamento alimentar.</p>',
   },
   {
     title: 'Evite a adição de sal durante as refeições (goDiet)',
-    text: `
-    Evite adicionar sal a sua refeição depois de pronta. Coloque sal, com moderação, apenas durante o preparo do alimento. Uma estratégia para reduzir a ingestão de sódio é o sal de ervas. Para preparar o seu sal de ervas, bata no liquidificador o sal e as ervas desidratadas, como alecrim, orégano, salsinha etc.
-
-    Você também pode utilizar outros temperos naturais nas suas refeições, como: alho, cebola, açafrão, curry, pimenta-do-reino, manjericão, orégano, salsinha, cebolinha, coentro, alecrim, louro, páprica, hortelã, manjerona, sálvia, estragão.
-    `,
+    text: '<p>Evite adicionar sal a sua refeição depois de pronta. Coloque sal, com moderação, apenas durante o preparo do alimento. Uma estratégia para reduzir a ingestão de sódio é o sal de ervas. Para preparar o seu sal de ervas, bata no liquidificador o sal e as ervas desidratadas, como alecrim, orégano, salsinha etc.</p><p>Você também pode utilizar outros temperos naturais nas suas refeições, como: alho, cebola, açafrão, curry, pimenta-do-reino, manjericão, orégano, salsinha, cebolinha, coentro, alecrim, louro, páprica, hortelã, manjerona, sálvia, estragão.</p>',
   },
 ];
